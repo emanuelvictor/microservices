4fbc2dbf373d887611857cb55aea1658920a975c
emanuelvictor <emanuelvictor@msn.com>
2022-09-07 00:33:20 -0300

    Creating Combination algorithm

--- api/functional/assessment/src/test/java/com/emanuelvictor/api/functional/assessment/CombinationTests.java
+++ api/functional/assessment/src/test/java/com/emanuelvictor/api/functional/assessment/CombinationTests.java
@@ -14,59 +14,108 @@ class CombinationTests {
     @Test
     void combineTest() {
         int[] ints = new int[]{7, 5, 6, 8, 9};
-        final Object[] combinations = new Object[ints.length];
+
+        System.out.println("Combinações possíveis: " + comb(ints.length));
 
         comb(ints);
+
+//        for (int i = 0; i < localCombinations.length; i++) {
+//            System.out.println(Arrays.toString(localCombinations[i]));
+//        }
     }
 
     int[] comb(final int[] ints) {
-        for (int c = 0; c < ints.length; c++) {
-            final int[] subArray = subArray(c, ints);
-            final int localCombinations[][] = new int[subArray.length][];
-            for (int i = 0; i < subArray.length; i++) {
-                localCombinations[i] = new int[subArray.length - i];
-                for (int j = subArray.length - i - 1; j >= 0; j--) {
-                    localCombinations[i][j] = subArray[j];
-                }
+        for (int i = 0; i < ints.length - 1; i++) { // run the counters
+            for (int j = 1; j < ints.length - i; j++) {
+                // print from the index I to index J + I
+                System.out.print(ints[0]);
+                System.out.println(" | " + Arrays.toString(subArrayInclude(ints, j, j + i)).replace("[", "").replace("]", ""));
             }
+            System.out.println(" ------ ");
+        }
+        return null;
+    }
 
-            for (int i = 0; i < localCombinations.length; i++) {
-                System.out.println(Arrays.toString(localCombinations[i]));
+    @Test
+    void subArrayIncludeTest() {
+        final int[] ints = new int[]{7, 5, 6, 8, 9};
+        Assertions.assertThat(subArrayInclude(ints, 1, 3)).isEqualTo(new int[]{5, 8});
+        Assertions.assertThat(subArrayInclude(ints, 0, 0)).isEqualTo(new int[]{7});
+    }
+
+    int[] subArrayInclude(final int[] array, final int... indexes) {
+
+        final int[] distinct = distinct(indexes);
+
+        int[] arrayReturn = new int[distinct.length];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < distinct.length; j++) {
+                if (i == distinct[j])
+                    arrayReturn[j] = array[distinct[j]];
             }
         }
 
-
-        return null;
+        return arrayReturn;
     }
+
+    @Test
+    void distinctTest() {
+        final int[] ints = new int[]{7, 5, 7, 8, 9};
+        Assertions.assertThat(distinct(ints)).isEqualTo(new int[]{7, 5, 8, 9});
+    }
+
+    public static int[] distinct(final int[] original) {
+
+        int[] distinct = new int[original.length];
+        int count = 0;
+        for (int k : original) {
+            boolean exists = false;
+            for (int j = 0; j < count; j++) {
+                if (distinct[j] == k) {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists) {
+                distinct[count++] = k;
+            }
+        }
+
+        distinct = Arrays.copyOf(distinct, count);
+
+        return distinct;
+
+    }
+
 
     /**
      *
      */
     @Test
-    public void subArrayTest() {
+    public void subArrayExcludeTest() {
         final int[] ints = new int[]{7, 5, 6, 8, 9};
-        Assertions.assertThat(subArray(new int[]{1, 3}, ints)).isEqualTo(new int[]{7, 6, 9});
+        Assertions.assertThat(subArrayExclude(ints, 1, 3)).isEqualTo(new int[]{7, 6, 9});
 
         final int[] ints2 = new int[]{7, 5, 6, 8, 9, 14, 4, 3, 1};
-        Assertions.assertThat(subArray(new int[]{1, 3, 4, 5}, ints2)).isEqualTo(new int[]{7, 6, 4, 3, 1});
+        Assertions.assertThat(subArrayExclude(ints2, 1, 3, 4, 5)).isEqualTo(new int[]{7, 6, 4, 3, 1});
 
-        Assertions.assertThat(subArray(new int[]{0}, ints2)).isEqualTo(new int[]{5, 6, 8, 9, 14, 4, 3, 1});
+        Assertions.assertThat(subArrayExclude(ints2, 0)).isEqualTo(new int[]{5, 6, 8, 9, 14, 4, 3, 1});
 
-        Assertions.assertThat(subArray(new int[]{0, 8}, ints2)).isEqualTo(new int[]{5, 6, 8, 9, 14, 4, 3});
+        Assertions.assertThat(subArrayExclude(ints2, 0, 8)).isEqualTo(new int[]{5, 6, 8, 9, 14, 4, 3});
 
-        Assertions.assertThat(subArray(new int[]{0, 2, 3, 4, 5, 6, 8}, ints2)).isEqualTo(new int[]{5, 3});
+        Assertions.assertThat(subArrayExclude(ints2, 0, 2, 3, 4, 5, 6, 8)).isEqualTo(new int[]{5, 3});
     }
 
-    int[] subArray(final int[] indexes, int[] array) {
+    int[] subArrayExclude(int[] array, final int... indexes) {
 
         for (int i = 0; i < indexes.length; i++) {
-            array = subArray(indexes[i] - i, array);
+            array = subArrayExclude(array, indexes[i] - i);
         }
 
         return array;
     }
 
-    int[] subArray(final int index, int[] array) {
+    int[] subArrayExclude(int[] array, final int index) {
 
         final int[] arrayReturn = new int[array.length - 1];
 
@@ -77,15 +126,6 @@ class CombinationTests {
                 arrayReturn[i - 1] = array[i];
         }
 
-        return arrayReturn;
-    }
-
-    int[] getRest(final int c, int[] array) {
-        final int[] arrayReturn = new int[array.length - c];
-//        for (int i = c; i < array.length; i++) {
-//            arrayReturn[i - c] = array[i];
-//        }
-        if (array.length - c >= 0) System.arraycopy(array, c, arrayReturn, 0, array.length - c);
         return arrayReturn;
     }
 
@@ -141,5 +181,15 @@ class CombinationTests {
         int pFac = fac(p);
         int x = nFac / (pFac * fac(n - p));
         return x + comb(n, p - 1);
+    }
+
+    @Test
+    void halfPlusOneTest() {
+        System.out.println(halfPlusOne(5));
+        System.out.println(halfPlusOne(6));
+    }
+
+    int halfPlusOne(int num) {
+        return num / 2 + 1;
     }
 }
