03137cca9406119925964b95b352fab423960170
Emanuel Victor <emanuelvictor@digix.com.br>
2023-03-20 07:59:28 -0300

    Implementing typedIdentifier

--- api/functional/flow-creator/src/main/java/com/emanuelvictor/api/functional/flowcreator/domain/services/generic/AbstractService.java
+++ api/functional/flow-creator/src/main/java/com/emanuelvictor/api/functional/flowcreator/domain/services/generic/AbstractService.java
@@ -1,6 +1,6 @@
 package com.emanuelvictor.api.functional.flowcreator.domain.services.generic;
 
-import com.emanuelvictor.api.functional.flowcreator.domain.entities.generic.UniqueIdentifier;
+import com.emanuelvictor.api.functional.flowcreator.domain.entities.generic.UniqueIdentifierJava;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,7 +24,7 @@ public abstract class AbstractService<T, ID extends Serializable> {
      *
      */
     @Autowired(required = false) // TODO couple, must use inject from jakarta
-    private JpaRepository<T, UniqueIdentifier<ID>> typedIdentifierRepository;
+    private JpaRepository<T, UniqueIdentifierJava<ID>> typedIdentifierRepository;
 
     public JpaRepository<T, ?> getRepository(){
         if(repository == null)
@@ -57,14 +57,14 @@ public abstract class AbstractService<T, ID extends Serializable> {
     /**
      * @param id {@link ID}
      */
-    public Optional<T> findById(final UniqueIdentifier<ID> id) {
+    public Optional<T> findById(final UniqueIdentifierJava<ID> id) {
         return typedIdentifierRepository.findById(id);
     }
 
     /**
      * @param id {@link ID}
      */
-    public void deleteById(final UniqueIdentifier<ID> id) {
+    public void deleteById(final UniqueIdentifierJava<ID> id) {
         typedIdentifierRepository.deleteById(id);
     }
 
--- api/functional/flow-creator/src/main/kotlin/com/emanuelvictor/api/functional/flowcreator/domain/entities/generic/UniqueIdentifier.kt
+++
@@ -1,8 +0,0 @@
-package com.emanuelvictor.api.functional.flowcreator.domain.entities.generic
-
-import java.io.Serializable
-
-open class UniqueIdentifier<ID> : Serializable {
-
-    open var id: ID? = null
-}
