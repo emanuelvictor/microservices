afa4773e5ad1257643bd90f39296ad7e3e21768c
Emanuel Victor <emanuelvictor@msn.com>
2023-12-01 19:29:16 -0300

    Incremmenting PAN FLOW

--- api/functional/berto-simulator/src/main/java/com/emanuelvictor/api/functional/bertosimulators/infrastructure/adapters/steps/pan/WaitUntilFinancialDataIsShowing.java
+++ api/functional/berto-simulator/src/main/java/com/emanuelvictor/api/functional/bertosimulators/infrastructure/adapters/steps/pan/WaitUntilFinancialDataIsShowing.java
@@ -5,6 +5,7 @@ import com.emanuelvictor.api.functional.bertosimulators.domain.ports.steps.pan.P
 import com.emanuelvictor.api.functional.bertosimulators.infrastructure.adapters.steps.AbstractStep;
 import com.emanuelvictor.api.functional.bertosimulators.infrastructure.browser.BrowserInstance;
 import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Wait;
@@ -12,6 +13,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.List;
+
 
 @Component
 @RequiredArgsConstructor
@@ -25,8 +28,8 @@ public class WaitUntilFinancialDataIsShowing extends AbstractStep implements Pan
         logger.info("waitUntilFinancialDataIsShowing");
         final Wait<WebDriver> wait = new WebDriverWait(browserInstance.getDriver(), Duration.ofSeconds(1000), Duration.ofSeconds(2));
         wait.until(d -> {
-            final WebElement element = browserInstance.getElementById("pan-mahoe-select-9");
-            return verifyElements(element);
+            final List<WebElement> elements = browserInstance.getDriver().findElements(By.id("pan-mahoe-select-9")); // TODO B.O aqui
+            return verifyElements(elements);
         });
         selectPAN5.execute(simulation);
     }
